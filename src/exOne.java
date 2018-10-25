46ccdc5d205b79e6005281fa91c6e75f834bda54
N1Kit <N9052233291@yandex.ru>
2018-10-25 12:23:45 +0300

    ex1

---
+++ src/exOne.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exOne {
+    public static void main(String[] args) {
+        String var;
+        float res, z = 10;
+        Scanner ix = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        float x = ix.nextFloat();
+        Scanner iy = new Scanner(System.in);
+        System.out.println("Введите второе число");
+        float y = iy.nextFloat();
+        if ((z-x) < (z-y)) {
+            var = "первое";
+            res = x;
+        }
+        else {
+            var = "второе";
+            res = y;
+        }
+        System.out.println("Ближайшее к " + z + " " + var + " введенное число " + res);
+    }
+
+}
